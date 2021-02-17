5787ae397ea45d239c34fcdc455e4a37e719b728
scottfjennings <scott.f.jennings@gmail.com>
2021-02-17 07:52:13 -0800

    separate functions for reading hepdata and hepsites

--- HEP_code/HEP_utility_functions.R
+++ HEP_code/HEP_utility_functions.R
@@ -1,6 +1,8 @@
 
-
 # a basic starting workflow for HEP data looks like this
+
+library(RODBC)
+
 
 #hep <- hep_from_access() %>% 
 #  clean_hep() %>% 
@@ -10,18 +12,43 @@
 #  trim_hep_columns()
 
 
+# default location, can be overwritten later
+#hepdata_location = "S:/Databases/HEP_data/HEPDATA.accdb"
+hepdata_location = "C:/Users/scott.jennings/Documents/Projects/HEP/HEP_data_work/HEP_data/HEPDATA.accdb"
 # data reading ----
+hep_from_access <- function(hepdata_location = hepdata_location){
+ db <- hepdata_location
+ con2 <- odbcConnectAccess2007(db)
 
-hep_from_access <- function(zdata.source = "HEP data", ztable = "tbl_HEPDATA") {
-library(RODBC)
-hep <- odbcConnect(dsn = zdata.source)
-hep_data <- sqlFetch(hep, ztable)
-close(hep)
-rm(hep)
-hep_data
+out_table <- sqlFetch(con2, "tbl_HEPDATA") 
+ 
+close(con2)
+
+return(out_table)
 }
 
-#hep_start <- hep_from_access()
+hep_sites_from_access <- function(hepdata_location = hepdata_location){
+ db <- hepdata_location
+ con2 <- odbcConnectAccess2007(db)
+
+out_table <- sqlFetch(con2, "tbl_HEPSITES") 
+ 
+close(con2)
+
+names(out_table) <- tolower(names(out_table))
+names(out_table) <- gsub("_", ".", names(out_table))
+out_table <- out_table %>% 
+  dplyr::select(code, parent.code, site.name, parent.site.name, atlas.code, county, subregion, old.name, utmnorth, utmeast) # %>% 
+  #mutate(code = as.character(code),
+  #       parent.code = as.character(parent.code),
+  #       site.name = as.character(site.name),
+  #       parent.site.name = as.character(parent.site.name))
+return(out_table)
+}
+
+# hep_start <- hep_from_access(hepdata_location)
+
+# hep_sites <- hep_sites_from_access(hepdata_location)
 
 # 2 data management functions ----
 
@@ -43,7 +70,7 @@ clean_hep <- function(hep) {
 
 # species order
 hep_species_order <- data.frame(species = c("GREG", "GBHE", "BCNH", "SNEG", "CAEG", "DCCO"),
-                                sp.order = seq(1:6)) 
+                                sp.order = seq(1:6))  
 
 # define_species_colors(); define standard species colors for plotting ----
 define_species_colors <- function(hep) {
@@ -63,7 +90,7 @@ add_site_names <- function(hep) {
   # input df must be the outpout of clean_hep()
   # requires tbl_HEPSites.csv to be saved in HEP_data/
   # should not change number of rows, should add 5 columns
-hep_sites <- hep_from_access(ztable = 'tbl_HEPSITES')
+hep_sites <- hep_sites_from_access(hepdata_location)
 names(hep_sites) <- tolower(names(hep_sites))
 names(hep_sites) <- gsub("_", ".", names(hep_sites))
 hep <- left_join(hep, dplyr::select(hep_sites, code, parent.code, site.name, parent.site.name, county, subregion)) %>% 
@@ -71,7 +98,7 @@ hep <- left_join(hep, dplyr::select(hep_sites, code, parent.code, site.name, par
          parent.code = as.character(parent.code),
          parent.site.name = as.character(parent.site.name),
          parent.site.name = ifelse(parent.site.name == "" & parent.code == 160, 'Cypress Road', parent.site.name)) %>% 
-  dplyr::select(code, parent.code, site.name, parent.site.name, everything(), -site)  %>% 
+  #dplyr::select(code, parent.code, site.name, parent.site.name, everything(), -site)  %>% 
     ungroup()
 }
 
@@ -121,7 +148,27 @@ trim_hep_columns <- function(hep, disturbance = TRUE, stage = TRUE, meta = TRUE)
 }
 
 
-# 3 calculation functions
+# 3 data summary functions ----
+hep_by_parent_site <- function(hep) {
+  hep_par_site <- hep %>% 
+    group_by(parent.code, year, species) %>% 
+    summarise(peakactvnsts = sum(peakactvnsts),
+              focalnests = sum(focalnests),
+              focfailure = sum(focfailure),
+              brd1 = sum(brd1),
+              brd2 = sum(brd2),
+              brd3 = sum(brd3),
+              brd4 = sum(brd4),
+              brd5 = sum(brd5),
+              brd6 = sum(brd6)) %>% 
+    ungroup() %>% 
+    mutate(parent.code = as.numeric(parent.code)) %>% 
+    add_site_names()
+  
+  
+}
+
+
 # calc_mean_brood_size(); calculate mean brood size by year, site, species ----
 calc_mean_brood_size <- function(hep) {
   # input df must be the outpout of clean_hep()
@@ -137,13 +184,14 @@ calc_mean_brood_size <- function(hep) {
   brd_longer <- brd_long[rep(row.names(brd_long), brd_long$num.nests), 1:4]	%>% 
     mutate(brdsz = as.numeric(brdsz)) %>% 
     ungroup()
-  
+
   mean_brd_size <- brd_longer %>% 
     group_by(year, code, species) %>% 
     summarise(mean.brd.size = mean(brdsz),
               sd.brd.size = sd(brdsz),
               brd.n.nests = n()) %>% 
     ungroup()
+  
   new_hep <- hep %>% 
     select(-brd1, -brd2, -brd3, -brd4, -brd5, -brd6) %>% 
     full_join(mean_brd_size) %>% 
@@ -151,24 +199,81 @@ calc_mean_brood_size <- function(hep) {
 }
 
 
+# calc_mean_brood_size(); calculate mean brood size by species, lumping any year and colonies in hep ----
+calc_mean_brood_size_lump_yr_col <- function(hep) {
+  # input df must be the outpout of clean_hep()
+  # should not change number of rows, should cut number of columns
+  # brd1-brd6 should be replaced by mean.brd.size and sd.brd.size
+  brd_long <- hep  %>% 
+  dplyr::select(year, code, species, brd1, brd2, brd3, brd4, brd5, brd6) %>% 
+    gather(brdsz, num.nests, -year, -code, -species) %>% 
+    filter(!(is.na(num.nests))) %>% 
+    mutate(brdsz = gsub("brd", "", brdsz)) %>% 
+    arrange(year, code, species, brdsz)
+  # make long again so there's a row for each nest of each brood size
+  brd_longer <- brd_long[rep(row.names(brd_long), brd_long$num.nests), 1:4]	%>% 
+    mutate(brdsz = as.numeric(brdsz)) %>% 
+    ungroup()
+
+  mean_brd_size <- brd_longer %>% 
+    group_by(species) %>% 
+    summarise(mean.brd.size = mean(brdsz),
+              sd.brd.size = sd(brdsz),
+              brd.n.nests = n()) %>% 
+    ungroup()
+  
+  new_hep <- hep %>% 
+    select(-brd1, -brd2, -brd3, -brd4, -brd5, -brd6) %>% 
+    full_join(mean_brd_size) %>% 
+    ungroup()
+}
+
+# annual percent change in colony size
+hep_annual_changer <- function(hep) {
+hep_changes <- hep %>% 
+  select(parent.site.name, species, year, subregion, peakactvnsts) %>% 
+  group_by(parent.site.name, species) %>% 
+  arrange(parent.site.name, species, year) %>%
+  mutate(prev.yr.nsts = lag(peakactvnsts),
+         #consec.yrs = ifelse(year - lag(year) == 1, 1, 0),
+         abs.change.1year = peakactvnsts - lag(peakactvnsts),
+         per.change.1year = ((peakactvnsts - prev.yr.nsts)/prev.yr.nsts)*100,
+         zero2zero = ifelse(peakactvnsts == 0 & prev.yr.nsts == 0, 1, 0),
+         zero2some = ifelse(prev.yr.nsts == 0 & peakactvnsts > 0, 1, 0)) %>% 
+  mutate(per.change.1year = ifelse(per.change.1year == Inf, (100 * abs.change.1year), per.change.1year))%>% 
+  #left_join(., spp.name) %>% 
+  #left_join(., hep.subreg.key) %>% 
+  ungroup()  %>% 
+  arrange(parent.site.name, species, year)
+}
+
 # function to calculate mean and sd for colony-level productivity parameters: brood size, proportion of nests successfull, colony productivity
-calc.repro=function(df) {
+calc_repro=function(df) {
 ##-- first peel off brood size columns to calculate average brood size and se
-brds <- select(df, CODE, YEAR, SPECIES, BRD1, BRD2, BRD3, BRD4, BRD5, BRD6)
-brds.long=melt(brds, id.vars = c("CODE", "YEAR", "SPECIES"), variable_name="br.size")
-brds.long$br.size <- as.numeric(substr(brds.long$br.size, 4, 4))
-brds.longer <- brds.long[rep(row.names(brds.long), brds.long$value), 1:4]
-brds.longer1=filter(brds.longer, br.size>0)
-brds.prod <- brds.longer %>%
-		group_by(CODE, YEAR, SPECIES) %>%
+brds <- df %>% 
+  select(code, year, species, brd1, brd2, brd3, brd4, brd5, brd6) %>% 
+  pivot_longer(cols = contains("brd"), names_to = "br.size", values_to = "num.nests") %>% 
+  mutate(br.size = gsub("brd", "", br.size)) %>% 
+  filter(!is.na(br.size), !is.na(num.nests), num.nests != 0)
+
+brds.expanded <- brds[rep(row.names(brds), brds$num.nests), ]
+
+
+
+brds.prod <- brds.expanded %>%
+  mutate(br.size = as.numeric(br.size)) %>% 
+		group_by(code, year, species) %>%
 		summarize(mean.brs=mean(br.size),
 				  se.brs=var(br.size)/sqrt(length(br.size)))
 ##-- then add mean and se brood size back to original df				  
-df1=merge(df, brds.prod, by.x=c("CODE", "YEAR", "SPECIES"), by.y=c("CODE", "YEAR", "SPECIES"), all.x=T)
+df1 <- full_join(df, brds.prod, by = c("code", "year", "species"))
 ##-- and finally calculate proportion and se of focal nests that were successful, and colony productivity (average number of chicks per nest attempt)
-df1$prop.successful=1-(df1$FOCFAILURE/df1$FOCALNESTS)
-df1$se.successfull=sqrt((df1$prop.successful*(1-df1$prop.successful))/df1$FOCALNESTS)
-df1$prod=df1$prop.successful*df1$mean.brs
+
+
+df1 <- df1 %>% 
+  mutate(prop.successful = 1-(focfailure/focalnests),
+         se.successfull = sqrt((prop.successful*(1-prop.successful))/focalnests),
+         prod = prop.successful*mean.brs)
 return(df1)
 }
 #hep=calc.repro(hep)
@@ -187,8 +292,9 @@ hep_colony_size_plotter <- function(colony.code, save.plot = TRUE) {
 col.name <- dplyr::distinct(hep4plot, site.name)[[1]] %>% 
   droplevels()
 
-year.lims = c(min(hep4plot$year), max(hep4plot$year))
-year.breaks = seq(year.lims[1], year.lims[2], by = round((year.lims[2]-year.lims[1])/3, 0))
+year.lims = c(floor(min(hep4plot$year)/5)*5, ceiling(max(hep4plot$year)/5)*5)
+year.breaks = seq(year.lims[1], year.lims[2], by = 5)
+#year.breaks = seq(year.lims[1], year.lims[2], by = round((year.lims[2]-year.lims[1])/3, 0))
 
 #-- 
 zcolor <- hep4plot %>% 
@@ -201,14 +307,15 @@ plot.cols <- zcolor[[2]]
   zplot = ggplot(data = hep4plot) +
     geom_point(aes(x = year, y = peakactvnsts, color = spp.name)) +
     stat_smooth(aes(x = year, y = peakactvnsts, color = spp.name), se = F) +
-    ggtitle(paste(col.name, " - Colony size", sep = "")) +
-    theme(legend.title=element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.5)) +
-    ylab("Peak colony size") +
+    #ggtitle(paste(col.name, " - Colony size", sep = "")) +
+    ggtitle(col.name) +
+    theme_classic() +
+    theme(legend.title=element_blank()) +
+    ylab("Number of nesting pairs") +
     xlab("") +
     scale_x_continuous(limits = year.lims, breaks = year.breaks) + 
-    scale_color_manual(values= plot.cols)
+    scale_color_manual(values= plot.cols) +
+    ylim(0, max(hep4plot$peakactvnsts)*1.1)
   if(save.plot == TRUE) {
     ggsave(paste("figures_output/HEP_by_colony_plots/", col.name, "_colonysize_", year.lims[1], "_", year.lims[2], ".jpg", sep = ""), width = 9, height = 6)
   }  else {
@@ -239,7 +346,7 @@ plot.cols <- zcolor[[2]]
   zplot = ggplot(data = hep4plot) +
     geom_point(aes(x = year, y = mean.brd.size, color = spp.name)) +
     stat_smooth(aes(x = year, y = mean.brd.size, color = spp.name), se = F) +
-    ggtitle(paste(col.name, " - Brood size", sep = "")) +
+    ggtitle(col.name) +
     theme(legend.title=element_blank(),
         panel.background = element_blank(),
         panel.border = element_rect(colour = "black", fill=NA, size=0.5)) +
