d95bdd0740c125e2078bcb89f8537896ab03510d
scottfjennings <scott.f.jennings@gmail.com>
2023-03-01 08:13:48 -0800

    edits for new downloads

--- HEP_code/get_HEP_PRISM.R
+++ HEP_code/get_HEP_PRISM.R
@@ -30,25 +30,50 @@ hep_parent_sites_dd_df <- hep_parent_sites_dd@coords %>%
 
 write.csv(hep_parent_sites_dd_df, "HEP_data/hep_parent_sites_dd_df.csv", row.names = F)
 
+read.csv("C:/Users/scott.jennings/Documents/Projects/core_monitoring_research/HEP/hep_analyses/how_are_the_egrets_doing/data/all_sfbbo_sites_subreg.csv") %>% 
+  dplyr::select(Latitude, Longitude) %>%
+  mutate(Latitude = as.numeric(Latitude),
+         Longitude = as.numeric(Longitude),
+         prism.index = row_number()) %>% 
+  write.csv("HEP_data/sfbbo_sites_dd_df.csv", row.names = F)
+
+
 # then bulk download data here: https://prism.oregonstate.edu/explorer/bulk.php
 # webpage allows you to upload the list of colony coords created above, select which variables to download for those site, select dates, and download.
+# NOTE, CSV CANNOT HAVE COLUMN HEADERS!!!!
 # can download monthly values in 15 year chunks, so will end up with multiple files
 # currently saving these to HEP_data/hep_prism_data
 
 
 # next, consolidate those files
 
-hep_prism_90_04 <- read.csv("HEP_data/hep_prism_data/PRISM_ppt_stable_4km_199001_200412.csv", skip = 10)
-hep_prism_05_19 <- read.csv("HEP_data/hep_prism_data/PRISM_ppt_stable_4km_200501_201912.csv", skip = 10)
-hep_prism_20_21 <- read.csv("HEP_data/hep_prism_data/PRISM_ppt_tmean_provisional_4km_202001_202112.csv", skip = 10)
+hep_prism_90_04 <- read.csv("HEP_data/hep_prism_data/hep_PRISM_ppt_stable_4km_199001_200412.csv", skip = 10)
+hep_prism_05_19 <- read.csv("HEP_data/hep_prism_data/hep_PRISM_ppt_stable_4km_200501_201912.csv", skip = 10)
+hep_prism_20_21 <- read.csv("HEP_data/hep_prism_data/hep_PRISM_ppt_tmean_provisional_4km_202001_202112.csv", skip = 10)
 
-hep_prism <- bind_rows(read.csv("HEP_data/hep_prism_data/PRISM_ppt_stable_4km_199001_200412.csv", skip = 10),
-                   read.csv("HEP_data/hep_prism_data/PRISM_ppt_stable_4km_200501_201912.csv", skip = 10),
-                   read.csv("HEP_data/hep_prism_data/PRISM_ppt_tmean_provisional_4km_202001_202112.csv", skip = 10)) %>% 
+hep_prism <- bind_rows(read.csv("HEP_data/hep_prism_data/hep_PRISM_ppt_stable_4km_199001_200412.csv", skip = 10),
+                   read.csv("HEP_data/hep_prism_data/hep_PRISM_ppt_stable_4km_200501_201912.csv", skip = 10),
+                   read.csv("HEP_data/hep_prism_data/hep_PRISM_ppt_tmean_provisional_4km_202001_202112.csv", skip = 10)) %>% 
   separate(Date, c("year", "month"), sep = "-", remove = F) %>% 
   rename(rain.mm = ppt..mm.) %>% 
   select(parent.code = Name, year, month, rain.mm)
 
+
+
+sfbbo_prism <- bind_rows(read.csv("HEP_data/hep_prism_data/sfbbo_PRISM_ppt_stable_4km_199501_200912.csv", skip = 10),
+                         read.csv("HEP_data/hep_prism_data/sfbbo_PRISM_ppt_stable_4km_201001_201901.csv", skip = 10),
+                         read.csv("HEP_data/hep_prism_data/sfbbo_PRISM_ppt_stable_4km_199001_199412.csv", skip = 10)) %>% 
+  rename("prism.index" = Name) %>% 
+  full_join(read.csv("C:/Users/scott.jennings/Documents/Projects/core_monitoring_research/HEP/hep_analyses/how_are_the_egrets_doing/data/all_sfbbo_sites_subreg.csv") %>%
+              dplyr::select(site.name) %>%
+              mutate(prism.index = row_number())) %>%
+  separate(Date, c("year", "month"), sep = "-", remove = F) %>% 
+  rename(rain.mm = ppt..mm.) %>% 
+  select(site.name, year, month, rain.mm)
+
 summary(hep_prism)
 
 saveRDS(hep_prism, "HEP_data/hep_prism_data/hep_prism_combined")         
+
+saveRDS(sfbbo_prism, "HEP_data/hep_prism_data/sfbbo_prism_combined")         
+
