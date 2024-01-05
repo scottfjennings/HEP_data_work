495baa53c82da170daeb1d8076e178b650e54dcc
scottfjennings <scott.f.jennings@gmail.com>
2024-01-05 08:48:49 -0800

    file paths for onedrive and more plots

--- HEP_code/HEP_colony_viewer.R
+++ HEP_code/HEP_colony_viewer.R
@@ -9,19 +9,19 @@ library(plotly)
 library(birdnames)
 
 options(scipen = 999)
-source("C:/Users/scott.jennings/Documents/Projects/core_monitoring_research/HEP/HEP_data_work/HEP_code/HEP_utility_functions.R")
+source("C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/core_monitoring_research/HEP/HEP_data_work/HEP_code/HEP_utility_functions.R")
 
 # data
 hep_sites <- readRDS(here("HEP_data/HEP_site_names_nums_utm"))
 
-
+abundance <- readRDS(here("HEP_data/hep_annual_nest_abundance"))
 
 # function to make the figure
 hep_colony_plotter <- function(zcolony) {
 
 # zcolony = c(27, 27.1)  
   
-abundance <- readRDS(here("HEP_data/hep_annual_nest_abundance")) %>% 
+abundance <- abunddance %>% 
   filter(code %in% zcolony) %>% 
   cut_never_nested() %>% 
   filter(peakactvnsts >= 0)
@@ -69,3 +69,42 @@ hep_colony_plotter(c(27, 27.1))
 
 
 ggsave(here("figures_output/Bolinas_colony_viewer.png"))
+
+
+abundance_by_spp <- abundance %>% 
+  filter(year == 2023, peakactvnsts > 0, species %in% c("GBHE", "GREG", "SNEG", "BCNH"))
+
+abundance_by_spp %>% 
+  ggplot() +
+  geom_bar(aes(x = peakactvnsts), width = 1) +
+  geom_bar(data = filter(abundance_by_spp, code == 48), aes(x = peakactvnsts), width = 2, fill = "red") +
+  scale_x_continuous(breaks = seq(0, 300, by = 50), labels = seq(0, 300, by = 50)) +
+  facet_wrap(~translate_bird_names(species, "alpha.code", "common.name")) +
+  labs(x = "Number of nests",
+       y = "Number of colonies",
+       title = "2023 size distribution of heron and egret colonies\nRed Rock colony shown in red") +
+  theme_bw()
+
+ggsave(here("figures_output/colony_size/Red Rock size comparison by species.png"), height = 6, width = 6)
+
+
+
+
+abundance_combined <- abundance %>% 
+  filter(year == 2023, peakactvnsts > 0, species %in% c("GBHE", "GREG", "SNEG", "BCNH")) %>%  
+  group_by(code, year) %>% 
+  summarise(peakactvnsts = sum(peakactvnsts)) %>% 
+  ungroup() 
+
+abundance_combined %>%
+  ggplot() +
+  geom_bar(aes(x = peakactvnsts), width = 1) +
+  geom_bar(data = filter(abundance_combined, code == 48), aes(x = peakactvnsts), width = 2, fill = "red") +
+  scale_x_continuous(breaks = seq(0, 300, by = 50), labels = seq(0, 300, by = 50)) +
+  labs(x = "Number of nests",
+       y = "Number of colonies",
+       title = "2023 size distribution of heron and egret colonies\n(all species combined)\nRed Rock colony shown in red") +
+  theme_bw()
+
+ggsave(here("figures_output/colony_size/Red Rock size comparison all species combined.png"), height = 6, width = 6)
+
